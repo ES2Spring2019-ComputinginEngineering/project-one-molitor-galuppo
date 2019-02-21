fc6ecaa7574b5522387fbc729f1960d5621d7156
Victoria Molitor <victoria.molitor@gmail.com>
2019-02-21 15:57:56 -0500

    Update PendulumSimulation.py

--- PendulumSimulation.py
+++ PendulumSimulation.py
@@ -6,9 +6,9 @@ def calc_PT():
     PT = 2*pi*(L/g)**(1/2)
     return PT
 
-def update_system(time1, time2):
+def update_system(ang,height,acc,vel,time1,time2):
     dt = time2-time1
-    NextAngle = (MaxAngle*sin((g/L)**(1/2))*dt) *180/pi                                                           #calculates the current angle to vertical of the pendulum based on the time elapsed
+    NextAngle = (ang*sin((g/L)**(1/2))*dt) *180/pi                                                           #calculates the current angle to vertical of the pendulum based on the time elapsed
     NextAccel = (-g/L) * sin(NextAngle)
     NextHeight = L - L*cos(NextAngle)
     NextVelocity = (2*g*L*(1-cos(NextAngle)))**(1/2)
@@ -26,17 +26,22 @@ MaxAngleDegrees = float(input("At what angle in degrees from vertical is the pen
 MaxAngle = MaxAngleDegrees *pi/180#converts degrees to radians
 g = 9.81                                                                                                              #force of gravity (m/s**2)
 time = np.linspace(0,20,21)
-ang = [0]
+ang = [90]
+height = [1]
 vel = [0]
+acc = [0]
 MaxHeight = L - L*cos(MaxAngle)
 
 
 i = 1
 while i < len(time):
-    print_system(1,1,1)
+    NextAngle, NextAccel, NextHeight, NextVelocity = update_system(ang[i-1], height[i-1], acc[i-1], vel[i-1], time[i-1], time[i])
+    print("Time:", time[i], "Angle: ", NextAngle, "Accel: ", NextAccel, "Height: ", NextHeight, "Velocity: ", NextVelocity)
     i += 1
-
+    ang.append(NextAngle)
+    vel.append(NextVelocity)
+    height.append(NextHeight)
+    acc.append(NextAccel)
 calc_PT()
 
-print(update_system(1,1.01))
 print(MaxHeight)
