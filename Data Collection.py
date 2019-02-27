695afa07c171f5a38d11529c11beb2e45eee3ed9
Victoria Molitor <victoria.molitor@gmail.com>
2019-02-27 16:05:26 -0500

    Create Data Collection.py

---
+++ Data Collection.py
@@ -0,0 +1,32 @@
+from microbit import *
+import math
+import random as r
+
+
+angle_time = []
+hourglass = Image("99999:09090:00900:09090:99999")
+
+while True:
+    display.scroll("Press A to Start!")
+    sleep(1000)
+    if button_a.was_pressed() == True:
+        start_time = running_time()
+        filename = "Pendulum Data"  + ".csv"
+        with open(filename, 'w') as file:
+            updated_time = 0
+            while updated_time < 5:
+                display.show(hourglass)
+                sleep(10)
+                x = accelerometer.get_x()
+                y = accelerometer.get_y()
+                angle = math.atan2(x,y)
+                finish_time = running_time()
+                elapsed_time1 = (finish_time - start_time)/1000
+                updated_time = elapsed_time1
+                file.write(str(elapsed_time1) + ", " + str(angle) + "\n")
+        display.scroll("Done!")
+
+
+with open('microbit.data', 'w') as my_file:
+    for i in angle_time:
+        my_file.write(str(i) + "\n")
