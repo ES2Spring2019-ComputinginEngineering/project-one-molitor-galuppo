7b4f16fd06c6789838fbd63e705b032d8303dc08
Victoria Molitor <victoria.molitor@gmail.com>
2019-03-05 11:34:13 -0500

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -2,10 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def calc_PT():
-    PT = 2*math.pi*(L/g)**(1/2)
-    return PT
-
 def update_system(pos,vel,acc,time1,time2): #updates the system based on the current values of position, velocity, and acceleration
     dt = time2-time1
     posNext = pos + vel*dt
@@ -13,17 +9,10 @@ def update_system(pos,vel,acc,time1,time2): #updates the system based on the cur
     accNext = (g/L)*math.cos(((math.pi)/2)-posNext)
     return posNext, velNext, accNext
 
-def print_system(time,pos,vel,acc):
-    print("Time:         ", time)
-    print("Position:     ", pos)
-    print("Velocity:     ", vel)
-    print("Acceleration: ", acc)
-   
-
 # Initial Conditions
 L = 1                                         #Sets the length of the pendulum in meters                                                    
 g = -9.81                                     #Force of gravity (m/s**2)
-time = np.linspace(0,20,20000)
+time = np.linspace(0,20,20000)                #Creates a set of times during which the position, velocity, and acceleration of the pendulum will be calculated 
 pos = [math.pi/6]                             #Sets the initial position in radians. This list is appended in the while loop to update the new position 
 vel = [0]                                     #Gives the initial velocity of 0. This list is appended in the while loop, where the velocity is updated based on acceleration and time. 
 acc = [(g/L)*math.cos((math.pi/2)-math.pi/6)] #Calculates the current angular acceleration of the pendulum, based on the starting angle. This list is appended in the while loop to add the new acceleration
@@ -36,7 +25,7 @@ while i < len(time):
     acc.append(accNext)
     i += 1
 
-
+#Plots the time elapsed (in seconds) and the anglular position of the pendulum (in radians)
 plt.subplot(3,1,1)
 plt.plot(time, pos, 'r--') 
 plt.xlabel('Time (seconds)')
@@ -45,7 +34,7 @@ plt.title('Angle vs Time')
 plt.xlim((0, 20)) # set x range to -1 to 8
 plt.grid()
 
-
+#Plots the time elapsed (in seconds) and the velocity of the pendulum (in radians per second)
 plt.subplot(3,1,2)
 plt.plot(time, vel, 'r--') 
 plt.xlabel('Time (seconds)')
@@ -54,7 +43,7 @@ plt.title('Velocity vs Time')
 plt.xlim((0, 20)) # set x range to -1 to 8
 plt.grid()
 
-
+#Plots the time elapsed (in seconds) and the angular acceleration of the pendulum (in radians per second per second)
 plt.subplot(3,1,3)
 plt.plot(time, acc, 'r--') 
 plt.xlabel('Time (seconds)')
