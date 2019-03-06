2705e1b265362820aeab3396664483396d1c311c
Victoria Molitor <victoria.molitor@gmail.com>
2019-03-06 15:59:01 -0500

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -26,6 +26,7 @@ pos = [math.pi/6]                                   #Sets the initial position i
 vel = [0]                                           #Gives the initial velocity of 0. This list is appended in the while loop, where the velocity is updated based on acceleration and time. 
 acc = [(g/L)*math.cos((math.pi/2)-math.pi/6)]       #Calculates the current angular acceleration of the pendulum, based on the starting angle. This list is appended in the while loop to add the new acceleration
 
+#Updates the system while time is less than 5 seconds and calculates the next velocity, position, and acceleration of the pendulum based on the change in position. 
 i = 1
 while i < len(time):
     posNext, velNext, accNext = update_system(pos[i-1], vel[i-1], acc[i-1], time[i-1], time[i])
@@ -42,7 +43,7 @@ print("The period time for a pendulum of length",L, "meters is", round(time2[max
 
 #Plots the time elapsed (in seconds) and the anglular position of the pendulum (in radians)
 plt.subplot(3,1,1)
-plt.plot(time, pos, 'r--', time2[maxima], position2[maxima], 'ko') 
+plt.plot(time, pos, 'r-', time2[maxima], position2[maxima], 'ko') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Angle (radians)')
 plt.title('Angle vs Time')
@@ -51,7 +52,7 @@ plt.grid()
 
 #Plots the time elapsed (in seconds) and the velocity of the pendulum (in radians per second)
 plt.subplot(3,1,2)
-plt.plot(time, vel, 'r--') 
+plt.plot(time, vel, 'r-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Velocity (rad/s)')
 plt.title('Velocity vs Time')
@@ -60,7 +61,7 @@ plt.grid()
 
 #Plots the time elapsed (in seconds) and the angular acceleration of the pendulum (in radians per second per second)
 plt.subplot(3,1,3)
-plt.plot(time, acc, 'r--') 
+plt.plot(time, acc, 'r-') 
 plt.xlabel('Time (seconds)')
 plt.ylabel('Acceleration (rad/s^2)')
 plt.title('Acceleration vs Time')
